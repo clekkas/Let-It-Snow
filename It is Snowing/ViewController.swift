6832d1e3364c123253e5371253c21a91c074e695
Chris Lekkas <clekkas@port-amcken01.corp.checkfree.com>
2018-12-17 17:16:38 -0800

    Initial Commit

---
+++ It is Snowing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  It is Snowing
+//
+//  Created by Lekkas, Chris (Hillsboro) on 12/17/18.
+//  Copyright © 2018 Lekkas, Chris (Hillsboro). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ It is SnowingTests/It_is_SnowingTests.swift
@@ -0,0 +1,34 @@
+//
+//  It_is_SnowingTests.swift
+//  It is SnowingTests
+//
+//  Created by Lekkas, Chris (Hillsboro) on 12/17/18.
+//  Copyright © 2018 Lekkas, Chris (Hillsboro). All rights reserved.
+//
+
+import XCTest
+@testable import It_is_Snowing
+
+class It_is_SnowingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
